aac890f02169ea94c8aacbded06fb720e55d3d3f
Ritesh <riteshkr254@gmail.com>
2020-02-03 10:10:13 +0530

    login

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { Router, Route, Switch, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import { history } from '../src/_helpers';
+import { alertActions } from '../src/_actions';
+import { PrivateRoute } from '../src/_components';
+import { HomePage } from '../src/HomePage';
+import { LoginPage } from '../src/LoginPage';
+import { RegisterPage } from '../src/RegisterPage';
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        history.listen((location, action) => {
+            // clear alert on location change
+            this.props.clearAlerts();
+        });
+    }
+
+    render() {
+        const { alert } = this.props;
+        return (
+            <div className="jumbotron">
+                <div className="container">
+                    <div className="col-sm-8 col-sm-offset-2">
+                        {alert.message &&
+                            <div className={`alert ${alert.type}`}>{alert.message}</div>
+                        }
+                        <Router history={history}>
+                            <Switch>
+                                <PrivateRoute exact path="/" component={HomePage} />
+                                <Route path="/login" component={LoginPage} />
+                                <Route path="/register" component={RegisterPage} />
+                                <Redirect from="*" to="/" />
+                            </Switch>
+                        </Router>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+function mapState(state) {
+    const { alert } = state;
+    return { alert };
+}
+
+const actionCreators = {
+    clearAlerts: alertActions.clear
+};
+
+const connectedApp = connect(mapState, actionCreators)(App);
+export { connectedApp as App };
